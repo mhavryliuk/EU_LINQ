c286db86d20aecc64ff2d6e99743ee55e57d6e1d
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-05-29 09:52:16 +0300

    Moved all methods to the class Student, added try-catch.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-/* Write a class Student with the following properties: first name, last name and age. 
+/** <remark>
+ * Write a class Student with the following properties: first name, last name and age. 
  * Write the method that for a given array of students first name and last name one in alphabetical order. Use LINQ.
  * 
  * Create a LINQ query that finds the first and the last name of all students, aged between 18 and 24 years including.  
@@ -10,23 +10,16 @@ using System.Linq;
  * 
  * By using the extension methods OrderBy(...) and ThenBy(...) with lambda expression and LINQ query sort a list of students 
  * by their first and last name in descending order. Write one method using LINQ another using lambda.
- * 
- * Напишите класс Student со следующими свойствами: имя, фамилия и возраст.
- * Напишите метод, который для данного массива студентов будет сортировать имя и фамилию в алфавитном порядке. Используйте LINQ.
- * 
- * Создайте запрос LINQ, который найдет имя и фамилию всех учащихся в возрасте от 18 до 24 лет включительно.
- * Используйте класс Student из предыдущего упражнения.
- * 
- * Используя методы расширения OrderBy (...) и ThenBy (...) с lambda-выражением и запросом LINQ, отсортируйте список студентов 
- * по их имени и фамилии в порядке убывания. Напишите один метод используя LINQ, другой используя lambda.
-*/
+</remark> */
 
 namespace _20180322_Task1_LINQ
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
+            Student st = new Student();
+
             List<Student> students = new List<Student>()
             {
                 new Student{FirstName = "Zahar", LastName = "Makalkin", Age = 21},
@@ -36,51 +29,25 @@ namespace _20180322_Task1_LINQ
                 new Student{FirstName = "Irina", LastName = "Sidorenko", Age = 9}
             };
 
-            // Сортируем имена и фамилии студентов по алфавиту.
-            var sortedStudents = from fl in students
-                                 orderby fl.FirstName, fl.LastName
-                                 select fl;
-
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Names of students in alphabetical order:");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            foreach (Student fl in sortedStudents)
-                Console.WriteLine(fl.FirstName + " " + fl.LastName);
-
-            // Поиск студентов в возрасте от 18 до 24 лет включительно.
-            var aged = from age in students
-                       where age.Age >= 18 && age.Age <= 24
-                       select age;
-
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("\nStudents aged 18 to 24 years inclusive:");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            foreach (Student age in aged)
-                Console.WriteLine(age.FirstName + " " + age.LastName + ", " + age.Age + " years old");
-
-            // LINQ: Сортируем имена и фамилии студентов по убыванию.
-            var sortLinq = from linqQuery in students
-                           orderby linqQuery.FirstName descending, linqQuery.LastName descending
-                           select linqQuery;
-
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("\nLINQ: names of students in descending order:");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            foreach (Student linqQuery in sortLinq)
-                Console.WriteLine(linqQuery.FirstName + " " + linqQuery.LastName);
-
-            // Lambda-expression: Сортируем имена и фамилии студентов по убыванию.
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("\nLambda-expression: names of students in descending order:");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            var sortLambda = students.OrderByDescending(std => std.FirstName);
-
-            foreach (Student item in sortLambda)
-                Console.WriteLine(item.FirstName + " " + item.LastName);
+            try
+            {
+                st.SortNamesInAlphabeticalOrder(students);
+                st.SearchStudentsAged18to24(students);
+                st.SortNamesInDescendingOrder_LINQ(students);
+                st.SortNamesInDescendingOrder_Lambda(students);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadKey();
         }
--- Student.cs
+++ Student.cs
@@ -1,9 +1,103 @@
-﻿namespace _20180322_Task1_LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20180322_Task1_LINQ
 {
-    public class Student
+    internal class Student
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
+
+        /// <summary>
+        /// The CeskForeNullandEmpts() method that checks the parameter to null and checks whether the parameter is an empty collection.
+        /// </summary>
+        /// <param name="students">Students list.</param>
+        private static void CheckForNullandEmpty(List<Student> students)
+        {
+            if (students == null)
+                throw new ArgumentNullException(nameof(students));
+            if (students.Count == 0)
+                throw new ArgumentException("Value cannot be an empty collection.", nameof(students));
+        }
+
+        /// <summary>
+        /// The SortingNamesInAlphabeticalOrder() method sorts the names and surnames of students alphabetically.
+        /// </summary>
+        /// <param name="students">Students list.</param>
+        internal void SortNamesInAlphabeticalOrder(List<Student> students)
+        {
+            CheckForNullandEmpty(students);
+
+            var sortedStudents = from fl in students
+                                 orderby fl.FirstName, fl.LastName
+                                 select fl;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Names of students in alphabetical order:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            foreach (Student fl in sortedStudents)
+                Console.WriteLine(fl.FirstName + " " + fl.LastName);
+        }
+
+        /// <summary>
+        /// The SearchStudentsAged18to24() method searches for students aged 18 to 24 years inclusive.
+        /// </summary>
+        /// <param name="students">Students list.</param>
+        internal void SearchStudentsAged18to24(List<Student> students)
+        {
+            CheckForNullandEmpty(students);
+
+            var aged = from age in students
+                       where age.Age >= 18 && age.Age <= 24
+                       select age;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nStudents aged 18 to 24 years inclusive:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            foreach (Student age in aged)
+                Console.WriteLine(age.FirstName + " " + age.LastName + ", " + age.Age + " years old");
+        }
+
+        /// <summary>
+        /// The SortNamesInDescendingOrder_LINQ() method sorts the names and surnames of students in descending order with using LINQ.
+        /// </summary>
+        /// <param name="students">Students list.</param>
+        internal void SortNamesInDescendingOrder_LINQ(List<Student> students)
+        {
+            CheckForNullandEmpty(students);
+
+            var sortLinq = from linqQuery in students
+                           orderby linqQuery.FirstName descending, linqQuery.LastName descending
+                           select linqQuery;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nLINQ: names of students in descending order:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            foreach (Student linqQuery in sortLinq)
+                Console.WriteLine(linqQuery.FirstName + " " + linqQuery.LastName);
+        }
+
+        /// <summary>
+        /// The SortNamesInDescendingOrder_LINQ() method sorts the names and surnames of students in descending order with using Lambda-expression.
+        /// </summary>
+        /// <param name="students">Students list.</param>
+        internal void SortNamesInDescendingOrder_Lambda(List<Student> students)
+        {
+            CheckForNullandEmpty(students);
+
+            var sortLambda = students.OrderByDescending(std => std.FirstName);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nLambda-expression: names of students in descending order:");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            foreach (Student item in sortLambda)
+                Console.WriteLine(item.FirstName + " " + item.LastName);
+        }
     }
 }
